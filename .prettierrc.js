7badc2e2d26ef5862fb84ad2ef1106dc5fb3c03f
ishiestar <ishiestar@gmail.com>
2020-09-16 19:58:13 +0530

    added settings

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto',
+  arrowParens: 'avoid',
+};
